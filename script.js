a565487d0882ed08ba481282f53f013532f8bd37
Sami Achnine <sachnine@my.waketech.edu>
2026-04-28 13:37:17 -0400

    so cooked

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-class Song {
-    constructor(albumCover, link, title, gameID, songLengthSecs) {
-        this.albumCover = "./covers/" + gameID + albumCover;
-        this.link = "./songs/" + gameID + link;
-        this.title = title;
+class Game {
+    constructor(gameID) {
         this.gameID = gameID;
+        this.songs = [];
+    }
+    addSong(self) {
+        
+    }
+}
+
+class Song extends Game {
+    constructor(albumCover, link, title, game, songLengthSecs) {
+        this.albumCover = "./covers/" + game.gameID + albumCover;
+        this.link = "./songs/" + game.gameID + link;
+        this.title = title;
+        this.game = super(game, []);
         this.songLengthSecs = songLengthSecs;
 
     }
@@ -15,5 +25,35 @@ class Song {
     }
     stop() {
         // ADD CODE FOR PAUSING -> SETTING SONG TO NULL OR BLANK OR DEFAULT OR WHATEVER
+    }
+}
+
+class Playlist {
+    constructor(albumCover, title, listID) {
+        this.albumCover = "./covers/" + listID + albumCover;
+        this.title = title;
+        this.listID = listID;
+        this.songs = this.getSongsFromGameId();
+    }
+    getSongsFromGameId(song) {
+        let gameID = song.gameID;
+        // for every song with that gameID, add it to the playlist
+        // ...but when are they declared?
+    }
+    shuffle() {
+        // ADD CODE FOR SHUFFLING ALL SONGS WITHIN this.songs
+    }
+}
+
+class UserPlaylist extends Playlist {
+    constructor(albumCover, title, listID) {
+        super(albumCover, title, listID);
+        this.songs = [];
+    }
+    addSong(song) {
+        this.songs.push(song);
+    }
+    shuffle() {
+        super.shuffle();
     }
 }
